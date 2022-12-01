8906c13b0e04a3399c787229efa537695fb18fae
Jeff Albaral <46907744+jalbaral@users.noreply.github.com>
2022-11-30 22:13:58 -0500

    current location feature added

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,9 +1,9 @@
-import 'mapbox-gl/dist/mapbox-gl.css';
-import React, { useRef, useEffect, useState } from 'react';
-import mapboxgl from 'mapbox-gl';
-import MapboxDirections from '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions';
-import '../../map.css';
-import toTheSunCoordinates from './testCoordinates.json';
+import "mapbox-gl/dist/mapbox-gl.css";
+import React, { useRef, useEffect, useState } from "react";
+import mapboxgl from "mapbox-gl";
+import MapboxDirections from "@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions";
+import "../../map.css";
+import toTheSunCoordinates from "./testCoordinates.json";
 
 mapboxgl.accessToken = `${process.env.REACT_APP_MAPBOX_KEY}`;
 
@@ -18,7 +18,7 @@ export default function Map() {
     if (map.current) return; // initialize map only once
     map.current = new mapboxgl.Map({
       container: mapContainer.current,
-      style: 'mapbox://styles/nutmegs1/clb2fo0ua000a14mj5t28j8c3',
+      style: "mapbox://styles/nutmegs1/clb2fo0ua000a14mj5t28j8c3",
       center: [lng, lat],
       zoom: zoom,
       pitch: 60,
@@ -26,53 +26,72 @@ export default function Map() {
     map.current.addControl(
       new MapboxDirections({
         accessToken: mapboxgl.accessToken,
+        profile: "mapbox/driving",
+        alternatives: false,
+        geometries: "geojson",
+        controls: { instructions: true },
+        flyTo: true,
       }),
-      'top-left'
+      "top-left"
     );
-
+    //current location
+    map.current.addControl(
+      new mapboxgl.GeolocateControl({
+        positionOptions: {
+          enableHighAccuracy: true,
+        },
+        // When active the map will receive updates to the device's location as it changes.
+        trackUserLocation: true,
+        // Draw an arrow next to the location dot to indicate which direction the device is heading.
+        showUserHeading: true,
+      }),
+      "top-left"
+    );
     //terrain
-    map.current.on('load', () => {
-      map.current.addSource('mapbox-dem', {
-        type: 'raster-dem',
-        url: 'mapbox://mapbox.mapbox-terrain-dem-v1',
+    map.current.on("load", () => {
+      map.current.addSource("mapbox-dem", {
+        type: "raster-dem",
+        url: "mapbox://mapbox.mapbox-terrain-dem-v1",
         tileSize: 512,
         maxZoom: 16,
       });
-      map.current.setTerrain({ source: 'mapbox-dem', exaggeration: 2.5 });
+      map.current.setTerrain({ source: "mapbox-dem", exaggeration: 2.5 });
       map.current.addLayer({
-        id: 'sky',
-        type: 'sky',
+        id: "sky",
+        type: "sky",
         paint: {
-          'sky-type': 'atmosphere',
-          'sky-atmosphere-sun': [0.0, 90.0],
-          'sky-atmosphere-sun-intensity': 15,
+          "sky-type": "atmosphere",
+          "sky-atmosphere-sun": [0.0, 90.0],
+          "sky-atmosphere-sun-intensity": 15,
         },
       });
     });
+    console.log("map.current.on({route})", map.current.on("route"));
+
     //route a trip
-    map.current.on('load', () => {
-      map.current.addSource('route', {
-        type: 'geojson',
+    map.current.on("load", () => {
+      map.current.addSource("route", {
+        type: "geojson",
         data: {
-          type: 'Feature',
+          type: "Feature",
           properties: {},
           geometry: {
-            type: 'LineString',
+            type: "LineString",
             coordinates: toTheSunCoordinates,
           },
         },
       });
       map.current.addLayer({
-        id: 'route',
-        type: 'line',
-        source: 'route',
+        id: "route",
+        type: "line",
+        source: "route",
         layout: {
-          'line-join': 'round',
-          'line-cap': 'round',
+          "line-join": "round",
+          "line-cap": "round",
         },
         paint: {
-          'line-color': '#000000',
-          'line-width': 8,
+          "line-color": "#000000",
+          "line-width": 8,
         },
       });
     });
@@ -80,7 +99,7 @@ export default function Map() {
 
   useEffect(() => {
     if (!map.current) return; // wait for map to initialize
-    map.current.on('move', () => {
+    map.current.on("move", () => {
       setLng(map.current.getCenter().lng.toFixed(4));
       setLat(map.current.getCenter().lat.toFixed(4));
       setZoom(map.current.getZoom().toFixed(2));
@@ -89,7 +108,7 @@ export default function Map() {
 
   return (
     <div>
-      <div ref={mapContainer} className='map-container' />
+      <div ref={mapContainer} className="map-container" />
     </div>
   );
 }
