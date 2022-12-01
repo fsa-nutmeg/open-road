e96c616626dbd96da182f8f9ab7dc6d88488a511
seth-way <seth.r.way@gmail.com>
2022-12-01 16:15:12 -0500

    seed 20 users, 25 comments, 4 routes

--- bin/seed.js
+++ bin/seed.js
@@ -1,8 +1,11 @@
 const db = require('../server/db');
 const { faker } = require('@faker-js/faker');
 const { getDatabase, ref, child, push, update } = require('firebase/database');
-
-function createUser(
+////////////////////////////////////////////////////////////////////////////////
+// CREATING USERS
+////////////////////////////////////////////////////////////////////////////////
+const createUser = (
+  refId,
   identifier,
   likedTrips,
   savedTrips,
@@ -10,9 +13,10 @@ function createUser(
   mileage,
   admin,
   badges
-) {
-  // A post entry.
-  const postData = {
+) => {
+  // A user entry.
+  const userData = {
+    refId: refId,
     identifier: identifier,
     likedTrips: likedTrips,
     savedTrips: savedTrips,
@@ -22,27 +26,23 @@ function createUser(
     badges: badges,
   };
 
-  // Get a key for a new Post.
-  const newPostKey = push(child(ref(db), 'users')).key;
+  // Get a key for a new User.
+  const newUserKey = push(child(ref(db), 'users')).key;
 
-  // Write the new post's data simultaneously in the posts list and the user's post list.
+  // Write the new user's data in the users list.
   const updates = {};
-  updates['/users/' + newPostKey] = postData;
+  updates['/users/' + newUserKey] = userData;
 
   update(ref(db), updates)
     .then(() => {
-      console.log('SUCCEESSSSSSS');
+      console.log('made user: ', userData.identifier);
     })
     .catch(err => {
       console.log(err);
     });
-}
+};
 
-createUser('email@email.com', [1, 2, 3], [3, 4, 5], [10], 10000000, true, [
-  'iron-booty',
-]);
-
-function random() {
+function randomArrayOfInts() {
   const size = Math.floor(Math.random() * 3);
   if (size === 0) {
     return [];
@@ -69,16 +69,19 @@ function randomBadges() {
   return result;
 }
 
+// Create 20 users
 for (let i = 0; i < 20; i++) {
+  const refId = i + 1;
   const identifier = faker.internet.email();
-  const likedTrips = random();
-  const savedTrips = random();
-  const takenTrips = random();
+  const likedTrips = randomArrayOfInts();
+  const savedTrips = randomArrayOfInts();
+  const takenTrips = randomArrayOfInts();
   const mileage = Math.floor(Math.random() * 999999999);
   const admin = false;
   const badges = randomBadges();
 
   createUser(
+    refId,
     identifier,
     likedTrips,
     savedTrips,
@@ -88,42 +91,106 @@ for (let i = 0; i < 20; i++) {
     badges
   );
 }
-
-/*
-mapboxNavigation.requestRoutes(
-    RouteOptions.builder()
-        .applyDefaultNavigationOptions()
-        .coordinatesList(listOf(originPoint, firstWaypoint, secondWaypoint, thirdWaypoint, destinationPoint))
-        .build()
-)
-*/
-
-/* ----------- TRIPS ------------ */
-function createTrip(tripData) {
+console.log('created all users!!!!!!!!!!!!\n\n');
+////////////////////////////////////////////////////////////////////////////////
+// CREATING TRIPS
+////////////////////////////////////////////////////////////////////////////////
+const createTrip = tripData => {
   // Get a key for a new Post.
   const newTripKey = push(child(ref(db), 'trips')).key;
 
-  // Write the new post's data simultaneously in the posts list and the user's post list.
+  // Write the new trip's data in the trips list.
   const updates = {};
   updates['/trips/' + newTripKey] = tripData;
 
   update(ref(db), updates)
     .then(() => {
-      console.log('SUCCEESSSSSSS');
+      console.log('made trip: ', tripData.name);
     })
     .catch(err => {
       console.log(err);
     });
-}
+};
 
-const goingToTheSunRoad = require('./trips/goingToTheSunRoad.json');
+let tripId = 1;
+
+const goingToTheSunRoad = {};
+
+goingToTheSunRoad.coordinates = require('./trips/goingToTheSunRoad.json');
 goingToTheSunRoad.name = 'Going-To-The-Sun Road';
 goingToTheSunRoad.owner = 1;
 goingToTheSunRoad.curveFactor = 5.5;
 goingToTheSunRoad.likes = [1, 3, 5, 44, 66];
 goingToTheSunRoad.featured = true;
+goingToTheSunRoad.refId = tripId++;
 
 createTrip(goingToTheSunRoad);
 
-//const goingToTheSunRoad = JSON.parse(require('./trips/goingToTheSunRoad.json'));
-//console.log(goingToTheSunRoad.weight_name + ': should be false?');
+const pacificCoastHighway = {};
+
+pacificCoastHighway.coordinates = require('./trips/pacificCoastHighway.json');
+pacificCoastHighway.name = 'Pacific Coast Highway';
+pacificCoastHighway.owner = 1;
+pacificCoastHighway.curveFactor = 4;
+pacificCoastHighway.likes = [1, 2, 3, 4, 5];
+pacificCoastHighway.featured = true;
+pacificCoastHighway.refId = tripId++;
+
+createTrip(pacificCoastHighway);
+
+const peakToPeakHighway = {};
+
+peakToPeakHighway.coordinates = require('./trips/peakToPeakHighway.json');
+peakToPeakHighway.name = 'Peak to Peak Highway';
+peakToPeakHighway.owner = 2;
+peakToPeakHighway.curveFactor = 3.9;
+peakToPeakHighway.likes = [1, 2, 7, 13];
+peakToPeakHighway.featured = true;
+peakToPeakHighway.refId = tripId++;
+
+createTrip(peakToPeakHighway);
+
+const guadalupeRiverRoad = {};
+
+guadalupeRiverRoad.coordinates = require('./trips/guadalupeRiverRoad.json');
+guadalupeRiverRoad.name = 'Guadalupe River Road';
+guadalupeRiverRoad.owner = 3;
+guadalupeRiverRoad.curveFactor = 2.9;
+guadalupeRiverRoad.likes = [1, 2, 3, 4];
+guadalupeRiverRoad.featured = true;
+guadalupeRiverRoad.refId = tripId++;
+
+createTrip(guadalupeRiverRoad);
+console.log('created all trips!!!!!!!!!!!!\n\n');
+////////////////////////////////////////////////////////////////////////////////
+// CREATING COMMENTS
+////////////////////////////////////////////////////////////////////////////////
+const createComment = commentData => {
+  // Get a key for a new Post.
+  const newCommentKey = push(child(ref(db), 'comments')).key;
+
+  // Write the new trip's data in the trips list.
+  const updates = {};
+  updates['/comments/' + newCommentKey] = commentData;
+
+  update(ref(db), updates)
+    .then(() => {
+      console.log('made comment: ', commentData.refId);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
+
+for (let i = 0; i < 25; i += 1) {
+  const comment = {};
+
+  comment.refId = i + 1;
+  comment.userId = Math.ceil(Math.random() * 20);
+  comment.routeId = Math.ceil(Math.random() * 4);
+  comment.text = faker.lorem.text();
+
+  createComment(comment);
+}
+
+console.log('created all comments!!!!!!!!!!!!\n\n');
