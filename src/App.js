5478e756928f9817d362927794c460f5cbd15f23
seth-way <seth.r.way@gmail.com>
2022-12-02 10:57:16 -0500

    create redux store with user slice

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
-import { Routes, Route } from "react-router-dom";
-import "./App.css";
-import Home from "./components/Home";
-import Login from "./components/Login";
-import Signup from "./components/Signup";
-import { UserAuthContextProvider } from "./context/userAuthContext";
-import MapPage from "./components/MapPage";
+import { Routes, Route } from 'react-router-dom';
+import './App.css';
+import Home from './components/Home';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import { UserAuthContextProvider } from './context/userAuthContext';
+import MapPage from './components/MapPage';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchUser } from './store/reducers/user';
+import { useEffect } from 'react';
 
 function App() {
+  /* COMMENTED CODE SUBSCRIBES TO STATE.USER
+     & FETCHES USER BY ID FROM DB
+     ----> WORKS AS IT SHOULD, BUT
+     ----> ONLY LEAVING FOR NOW FOR AN EXAMPLE
+  const dispatch = useDispatch();
+  const { user } = useSelector(state => state.user);
+  
+  useEffect(() => {
+    dispatch(fetchUser('-NIEWXnrYaDidApReiA-'));
+  });
+  */
   return (
     <UserAuthContextProvider>
       <Routes>
-        <Route path="/home" element={<Home />} />
-        <Route path="/" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/map" element={<MapPage />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/map' element={<MapPage />} />
       </Routes>
     </UserAuthContextProvider>
   );
