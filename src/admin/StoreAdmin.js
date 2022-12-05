3921a385cd27dd8253f437b3e23357e2ceaa6e9c
Riley Mack <riley.j.mack@gmail.com>
2022-12-05 17:26:35 -0500

    admin almost has api

--- src/admin/StoreAdmin.js
+++ src/admin/StoreAdmin.js
@@ -1,16 +1,30 @@
-import { Admin, Resource, EditGuesser } from "react-admin";
+import { Admin, Resource, EditGuesser, ListGuesser } from "react-admin";
 import jsonServerProvider from "ra-data-json-server";
 import { UserList } from "./users";
 import { PostList } from "./posts";
 import Dashboard from "./Dashboard";
+import {
+  CommentList,
+  CommentShow,
+  CommentCreate,
+  CommentEdit,
+} from "./comments";
 
-const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
+import { dataProvider, authProvider } from "../firebase-config";
 
 function StoreAdmin() {
   return (
     <Admin basename="/admin" dashboard={Dashboard} dataProvider={dataProvider}>
-      <Resource name="users" list={UserList} recordRepresentation="name" />
+      <Resource name="users" list={UserList} />
       <Resource name="posts" list={PostList} edit={EditGuesser} />
+      <Resource
+        name="comments"
+        // icon={CommentIcon}
+        list={CommentList}
+        show={CommentShow}
+        create={CommentCreate}
+        edit={CommentEdit}
+      />
     </Admin>
   );
 }
--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,6 +1,12 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import firebase from "firebase/app";
+
 import { getDatabase } from "firebase/database";
+import { FirebaseAuthProvider } from "react-admin-firebase";
+import "firebase/firestore";
+import "firebase/database";
+import firebaseDataProvider from "ra-data-firebase-client";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -18,9 +24,15 @@ const app = initializeApp(firebaseConfig);
 // Initialize services
 export const auth = getAuth(app);
 const database = getDatabase(app);
+const options = {
+  // Use a different root document to set your resource collections, by default it uses the root collections of firestore
+  // Your own, previously initialized firebase app instance
+  // app: database,
+};
+const settings = { context: "dev", imagekey: "images", filekey: "files" };
 
-
-
-
+export const dataProvider = firebaseDataProvider(firebaseConfig, settings);
+export const authProvider = FirebaseAuthProvider(firebaseConfig, settings);
+// export const firebaseRealtime = FirebaseRealTimeSaga(dataProvider);
 
 export default app;
