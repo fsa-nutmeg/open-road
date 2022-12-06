641228e46f4fc863a35e4ca398e88f93ce31b975
Riley Mack <riley.j.mack@gmail.com>
2022-12-06 11:23:17 -0500

    alltrips

--- src/components/AllTrips.js
+++ src/components/AllTrips.js
@@ -1,17 +1,18 @@
-import React, { useState, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Navigate } from 'react-router-dom';
-import { fetchAllTrips } from '../store/reducers/allTrips';
-import Navbar from './Navbar';
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
+import { fetchAllTrips } from "../store/reducers/allTrips";
+import Navbar from "./Navbar";
 
 const AllTrips = () => {
   const dispatch = useDispatch();
   dispatch(fetchAllTrips());
 
-  const { allTrips } = useSelector(state => state.allTrips);
+  const { allTrips } = useSelector((state) => state.allTrips);
   const [redirect, redirectTo] = useState(false);
 
-  const handleClick = e => {
+  const handleClick = (e) => {
+    console.log(e.target.id);
     redirectTo(e.target.id);
   };
 
@@ -20,7 +21,7 @@ const AllTrips = () => {
   return (
     <div>
       <Navbar />
-      {allTrips.map(trip => (
+      {allTrips.map((trip) => (
         <div key={trip.id} onClick={handleClick} id={trip.id}>
           {trip.name}
         </div>
