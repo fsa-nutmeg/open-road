70ac6a812e8e72bf0cc7054a410940fa8dff28ad
Ashton Griffin <ashtongriffin11@gmail.com>
2022-12-06 14:42:12 -0500

    took logout off home

--- src/components/Home.js
+++ src/components/Home.js
@@ -19,15 +19,6 @@ const Home = () => {
     <>
       <Navbar />
       <img src="https://www.mensjournal.com/wp-content/uploads/2018/07/Americas.jpg?quality=86&strip=all" />
-
-      <div className="p-4 box mt-3 text-center">
-        Hello Welcome <br />
-      </div>
-      <div className="d-grid gap-2">
-        <Button variant="primary" onClick={handleLogout}>
-          Log out
-        </Button>
-      </div>
     </>
   );
 };
