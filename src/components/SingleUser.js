f85aa6f022851b7684069cd9c244171a0e66e54a
Ashton Griffin <ashtongriffin11@gmail.com>
2022-12-06 13:06:38 -0500

    added user page and navbar functionality

--- src/components/SingleUser.js
+++ src/components/SingleUser.js
@@ -1,8 +1,28 @@
 import React from 'react'
+import Navbar from './Navbar'
+import { useNavigate } from "react-router-dom";
+import { useUserAuth } from "../context/userAuthContext";
 
 const SingleUser = () => {
+  const { logOut, user } = useUserAuth();
+  const navigate = useNavigate();
+  const handleLogout = async () => {
+    try {
+      await logOut();
+      navigate("/");
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
-    <div>SingleUser</div>
+    <div>
+      <Navbar />
+      SingleUser
+      <button className='text-red-800' onClick={handleLogout}>
+          Log out
+        </button>
+      </div>
   )
 }
 
